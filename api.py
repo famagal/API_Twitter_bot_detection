a4fd9e8d86f82d45ce55fe6ff6d705dabb815415
Fabio Alves <famagal@hotmail.com>
2021-12-02 15:17:20 -0300

    testing 3

--- api.py
+++ api.py
@@ -20,14 +20,14 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 gcs_path_user = 'gs://tweet-project-713/models/twitter_bot_detector/v1/Logit_opt.joblib'
-#gcs_path_text = 'gs://tweet-project-713/models/twitter_bot_detector/v1/RNN_BIG.h5'
-#gcs_path_w2v = 'gs://tweet-project-713/data/word2vec.joblib'
+gcs_path_text = 'gs://tweet-project-713/models/twitter_bot_detector/v1/RNN_BIG.h5'
+gcs_path_w2v = 'gs://tweet-project-713/data/word2vec.joblib'
 
 model_user = joblib.load(tf.io.gfile.GFile(gcs_path_user, 'rb'))
 
-#model_text = load_model(h5py.File(tf.io.gfile.GFile(gcs_path_text, 'rb'), 'r'))
+model_text = load_model(h5py.File(tf.io.gfile.GFile(gcs_path_text, 'rb'), 'r'))
 
-#word2vec = joblib.load(tf.io.gfile.GFile(gcs_path_w2v, 'rb'))
+word2vec = joblib.load(tf.io.gfile.GFile(gcs_path_w2v, 'rb'))
 
 
 # define a root `/` endpoint
@@ -36,8 +36,8 @@ def predict(username):
     user = user_data_request(username)
     tweets = tweet_data_request(user)
     X_user = user_preprocessing(tweets, user)
-    #X_tweet = tweet_preprocessing(tweets, word2vec)
+    X_tweet = tweet_preprocessing(tweets, word2vec)
     pred_user = model_user.predict(X_user)
-    #pred_text = np.mean(model_text.predict(X_tweet))
-    #return {'user level prediction': str(pred_user[0]), 'tweet_level_prediction': str(pred_text)}
-    return {'username' : str(pred_user)}
+    pred_text = np.mean(model_text.predict(X_tweet))
+    return {'user level prediction': str(pred_user[0]), 'tweet_level_prediction': str(pred_text)}
+    #return {'username' : str(pred_user)}
